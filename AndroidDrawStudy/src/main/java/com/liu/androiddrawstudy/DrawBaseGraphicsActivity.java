4a9cae9078680ad453c7480eaa322a8c38c310b9
liukangqi2015 <330620143@qq.com>
2016-12-02 18:48:40 +0800

    初次提交AndroidDrawStudy

---
+++ AndroidDrawStudy/src/main/java/com/liu/androiddrawstudy/DrawBaseGraphicsActivity.java
@@ -0,0 +1,17 @@
+package com.liu.androiddrawstudy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 绘制基本图形
+ * Created by liu on 2016/12/1.
+ */
+public class DrawBaseGraphicsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actiivty_draw_base_graphics);
+    }
+}
--- AndroidDrawStudy/src/main/java/com/liu/androiddrawstudy/MainActivity.java
+++ AndroidDrawStudy/src/main/java/com/liu/androiddrawstudy/MainActivity.java
@@ -1,13 +1,38 @@
 package com.liu.androiddrawstudy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button draw_base_graphics_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setListener();
+    }
+
+    private void initView() {
+        draw_base_graphics_btn= (Button) findViewById(R.id.draw_base_graphics_btn);
+    }
+
+    private void setListener() {
+        draw_base_graphics_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.draw_base_graphics_btn:
+                startActivity(new Intent(MainActivity.this,DrawBaseGraphicsActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
