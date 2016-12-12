7ebb6be029ef6328609f7f513c75d6fe579437c2
liu <330620143@qq.com>
2016-12-12 17:13:10 +0800

    添加drawPoint演示

--- AndroidDrawStudy/src/main/java/com/liu/androiddrawstudy/DrawBaseGraphicsActivity.java
+++ AndroidDrawStudy/src/main/java/com/liu/androiddrawstudy/DrawBaseGraphicsActivity.java
@@ -53,6 +53,7 @@ public class DrawBaseGraphicsActivity extends AppCompatActivity{
                 return true;
             }
         });
+        //默认选中第一项
         mViewNavigation.setCheckedItem(R.id.draw_point);
         mViewNavigation.getMenu().performIdentifierAction(R.id.draw_point, 0);
 
