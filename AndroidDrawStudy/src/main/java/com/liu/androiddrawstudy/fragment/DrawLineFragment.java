b4eb52fc71fd8f8eac3fe4cd8ca5eedc0ac1c165
lkq <330620143@qq.com>
2016-12-19 16:59:43 +0800

    添加画线的例子，简单封装了Fragment

--- AndroidDrawStudy/src/main/java/com/liu/androiddrawstudy/fragment/DrawLineFragment.java
+++ AndroidDrawStudy/src/main/java/com/liu/androiddrawstudy/fragment/DrawLineFragment.java
@@ -1,11 +1,6 @@
 package com.liu.androiddrawstudy.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.liu.androiddrawstudy.R;
 
